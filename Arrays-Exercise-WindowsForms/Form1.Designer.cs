1284049465434944c6c4ab4a92266c8d5e4bb227
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-01 15:11:24 +0200

    Add project files.

---
+++ Arrays-Exercise-WindowsForms/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Arrays_Exercise_WindowsForms
+{
+    partial class FrmStudentMarks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGrades = new System.Windows.Forms.Button();
+            this.LstInfo = new System.Windows.Forms.ListBox();
+            this.BtnPassFail = new System.Windows.Forms.Button();
+            this.BtnHighestMark = new System.Windows.Forms.Button();
+            this.BtnFrequency = new System.Windows.Forms.Button();
+            this.BtnShowSorted = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnGrades
+            // 
+            this.BtnGrades.Location = new System.Drawing.Point(67, 12);
+            this.BtnGrades.Name = "BtnGrades";
+            this.BtnGrades.Size = new System.Drawing.Size(199, 62);
+            this.BtnGrades.TabIndex = 0;
+            this.BtnGrades.Text = "Grades";
+            this.BtnGrades.UseVisualStyleBackColor = true;
+            this.BtnGrades.Click += new System.EventHandler(this.BtnGrades_Click);
+            // 
+            // LstInfo
+            // 
+            this.LstInfo.FormattingEnabled = true;
+            this.LstInfo.ItemHeight = 16;
+            this.LstInfo.Location = new System.Drawing.Point(407, 104);
+            this.LstInfo.Name = "LstInfo";
+            this.LstInfo.Size = new System.Drawing.Size(340, 260);
+            this.LstInfo.TabIndex = 1;
+            // 
+            // BtnPassFail
+            // 
+            this.BtnPassFail.Location = new System.Drawing.Point(67, 93);
+            this.BtnPassFail.Name = "BtnPassFail";
+            this.BtnPassFail.Size = new System.Drawing.Size(198, 66);
+            this.BtnPassFail.TabIndex = 2;
+            this.BtnPassFail.Text = "Show Pass / Fail";
+            this.BtnPassFail.UseVisualStyleBackColor = true;
+            this.BtnPassFail.Click += new System.EventHandler(this.BtnPassFail_Click);
+            // 
+            // BtnHighestMark
+            // 
+            this.BtnHighestMark.Location = new System.Drawing.Point(71, 185);
+            this.BtnHighestMark.Name = "BtnHighestMark";
+            this.BtnHighestMark.Size = new System.Drawing.Size(194, 63);
+            this.BtnHighestMark.TabIndex = 3;
+            this.BtnHighestMark.Text = "Highest Marks";
+            this.BtnHighestMark.UseVisualStyleBackColor = true;
+            this.BtnHighestMark.Click += new System.EventHandler(this.BtnHighestMark_Click);
+            // 
+            // BtnFrequency
+            // 
+            this.BtnFrequency.Location = new System.Drawing.Point(77, 272);
+            this.BtnFrequency.Name = "BtnFrequency";
+            this.BtnFrequency.Size = new System.Drawing.Size(188, 67);
+            this.BtnFrequency.TabIndex = 4;
+            this.BtnFrequency.Text = "Show Frequency";
+            this.BtnFrequency.UseVisualStyleBackColor = true;
+            this.BtnFrequency.Click += new System.EventHandler(this.BtnFrequency_Click);
+            // 
+            // BtnShowSorted
+            // 
+            this.BtnShowSorted.Location = new System.Drawing.Point(79, 359);
+            this.BtnShowSorted.Name = "BtnShowSorted";
+            this.BtnShowSorted.Size = new System.Drawing.Size(186, 65);
+            this.BtnShowSorted.TabIndex = 5;
+            this.BtnShowSorted.Text = "Show Sorted";
+            this.BtnShowSorted.UseVisualStyleBackColor = true;
+            this.BtnShowSorted.Click += new System.EventHandler(this.BtnShowSorted_Click);
+            // 
+            // FrmStudentMarks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnShowSorted);
+            this.Controls.Add(this.BtnFrequency);
+            this.Controls.Add(this.BtnHighestMark);
+            this.Controls.Add(this.BtnPassFail);
+            this.Controls.Add(this.LstInfo);
+            this.Controls.Add(this.BtnGrades);
+            this.Name = "FrmStudentMarks";
+            this.Text = "Student Marks";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGrades;
+        private System.Windows.Forms.ListBox LstInfo;
+        private System.Windows.Forms.Button BtnPassFail;
+        private System.Windows.Forms.Button BtnHighestMark;
+        private System.Windows.Forms.Button BtnFrequency;
+        private System.Windows.Forms.Button BtnShowSorted;
+    }
+}
+
---
+++ Arrays-Exercise-WindowsForms/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays_Exercise_WindowsForms
+{
+    public partial class FrmStudentMarks : Form
+    {
+        public FrmStudentMarks()
+        {
+            InitializeComponent();
+        }
+
+        // We initialize empty arrays to store the input data
+        string[] studentName = new string[101];
+        int[] studentMark = new int[101];
+        int[] frequency = new int[101];
+
+        // We initialize counter to count number of entries
+        // and we pass it explicitly with the -ref modifier
+        // in order to act as reference type and keep it's modified value.
+        int counter = 0;
+
+        private void BtnGrades_Click(object sender, EventArgs e)
+        {
+            // We pass the empty arrays and fill them with data
+            DataEntry(studentName, studentMark, ref counter);
+            Display(studentName, studentMark, counter);
+            
+        }
+        private void BtnPassFail_Click(object sender, EventArgs e)
+        {
+            PassFail(studentName, studentMark, counter);
+        }
+        private void BtnHighestMark_Click(object sender, EventArgs e)
+        {
+            ShowHighestMark(studentName, studentMark, counter);
+        }
+        private void BtnFrequency_Click(object sender, EventArgs e)
+        {
+            GetMarksFrequency(studentMark, frequency, counter);
+            ShowFrequency(frequency);
+        }
+        private void BtnShowSorted_Click(object sender, EventArgs e)
+        {
+            ShowSorted(studentMark, studentName, counter);
+        }
+
+
+        private void DataEntry(string[] stName, int[] stMark, ref int count)
+        {
+            while (true)
+            {
+                string response = "";
+
+                do
+                {
+                    response = Microsoft.VisualBasic.Interaction.InputBox("Do you want to enter info y/n?", "Info");
+                }
+                while (response != "y" && response != "n");
+
+                if (response == "n")
+                    break;
+
+                string tempS = Microsoft.VisualBasic.Interaction.InputBox("Enter student name: ", "Student name");
+                int tempM = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter student mark: ", "Student mark"));
+
+                if (String.IsNullOrWhiteSpace(tempS) || (tempM < 0 || tempM > 100))
+                {
+                    MessageBox.Show("Invalid entry", "Error", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    // We start from index 1
+                    count++;
+                    stName[count] = tempS;
+                    stMark[count] = tempM;
+                }
+            }           
+        }
+
+        private void Display(string[] names, int[] marks, int count)
+        {
+            LstInfo.Items.Clear();
+            LstInfo.Items.Add("List of students / marks.");
+            for (int i = 1; i < count + 1; i++)
+            {
+                LstInfo.Items.Add(names[i] + "\t" + marks[i]);
+            }
+        }
+
+        private void PassFail(string[] names, int[] marks, int count)
+        {
+            LstInfo.Items.Clear();
+            for (int i = 1; i < count + 1; i++)
+            {
+                if (marks[i] >= 50)
+                {
+                    LstInfo.Items.Add(names[i] + " " + marks[i] + "\t Pass");
+                }
+                else
+                {
+                    LstInfo.Items.Add(names[i] + " " + marks[i] + "\t Fail");
+                }      
+            }
+        }
+
+        
+
+        private void ShowHighestMark(string[] names, int[] marks, int count)
+        {
+            LstInfo.Items.Clear();
+
+            int highest = marks[1];
+            for (int i = 1; i < count + 1; i++)
+            {
+                if (marks[i] > highest)
+                    highest = marks[i]; 
+            }
+            LstInfo.Items.Add("Highest mark is: " + highest);
+            LstInfo.Items.Add("Students with highest marks: ");
+            for (int i = 1; i < count + 1; i++)
+            {
+                if (marks[i] == highest)
+                {
+                    LstInfo.Items.Add(names[i]);
+                }
+            }
+        }
+
+        // We use array counter to count the frequency
+        private void GetMarksFrequency(int[] marks, int[] frequency, int count)
+        { 
+            for (int i = 1; i < count + 1; i++)
+            {
+                frequency[marks[i]] = frequency[marks[i]] + 1;
+            }
+        }
+
+        private void ShowFrequency(int[] f)
+        {
+            LstInfo.Items.Clear();
+            for (int i = 1; i < f.Count(); i++)
+            {
+                if (f[i] != 0)
+                    LstInfo.Items.Add(i + " " + f[i].ToString());
+            }
+        }
+
+        // We use bubble sort to sort the marks and names
+        private void ShowSorted(int[] marks, string[] names, int count)
+        {
+            // Flag to keep track if swap is made
+            bool flag = false;
+
+            // Declare new arrays and copy the data into them,
+            // so we don't change the intial ones.
+            int[] marksSorted = new int[marks.Count()];
+            string[] namesSorted = new string[names.Count()];
+
+            Array.Copy(marks, marksSorted, marks.Count());
+            Array.Copy(names, namesSorted, names.Count());
+
+            
+            do
+            {
+                for (int i = 1; i <= count - 1; i++)
+                {
+                    flag = false;
+                    int temp = 0;
+                    string tempN = "";
+                    // If first number is bigger than the second we swap them,
+                    // and we also swap the names in the "names" array
+                    if (marksSorted[i] > marksSorted[i + 1])
+                    {
+                        temp = marksSorted[i];
+                        marksSorted[i] = marksSorted[i + 1];
+                        marksSorted[i + 1] = temp;
+
+                        tempN = namesSorted[i];
+                        namesSorted[i] = namesSorted[i + 1];
+                        namesSorted[i + 1] = tempN;
+                        flag = true;
+                    }
+                }
+            }
+            while (flag);
+
+            LstInfo.Items.Clear();
+            for (int i = 1; i <= count; i++)
+            {
+                LstInfo.Items.Add(marksSorted[i] + "\t" + namesSorted[i]);
+            }
+        }
+    }
+}
